0c0aa4bb4caa5a1494b6e48ec43e58aa5025e3e4
John Meade <johnnynibbles@gmail.com>
2018-08-11 18:21:38 -0400

    start of the home page

--- src/DartCalendar.Web/ClientApp/pages/home/home.ts
+++ src/DartCalendar.Web/ClientApp/pages/home/home.ts
@@ -3,5 +3,26 @@ import Component from "vue-class-component";
 
 @Component
 export default class AppComponent extends Vue {
+    dartEvents: Array<DartEvent>;
     name: string = "component-data";
+
+    constructor(){
+        super();
+        this.dartEvents = new Array<DartEvent>();
+    }
+
+    bind(){
+        this.dartEvents.push(new DartEvent("event 1", "address 1"));
+        this.dartEvents.push(new DartEvent("event 2", "address 2"));
+        this.dartEvents.push(new DartEvent("event 3", "address 3"));
+    }
+}
+
+class DartEvent {
+    name: string;
+    address: string;
+    constructor(name: string, address: string){
+        this.name = name;
+        this.address = address;
+    }
 }
--- src/DartCalendar.Web/ClientApp/pages/home/home.vue
+++ src/DartCalendar.Web/ClientApp/pages/home/home.vue
@@ -1,15 +1,39 @@
 <template>
-  <div>
-    <div class="row">
-	  <div class="col-sm-12">
-        <input input="text" />
-	  </div>
+  <div class="border-it">
+    <div class="row search-row">
+      <div class="col-sm">
+        <div>
+          <form class="form form-inline">
+            <input input="text" class="form-control" placeholder="" />
+            <button class="btn btn-success">Search</button>
+          </form>
+        </div>
+      </div>
     </div>
     <div class="row">
-	  <div class="col-sm-6">Event Name</div>
-	  <div class="col-sm-6">Event Address</div>
+      <div class="col-sm-10">
+       <div  style="background-color: Red;">
+        <div v-for="dartEvent in dartEvents">
+            <div>{{dartEvent.name}}</div>
+          </div>
+       </div>
+      </div>
+      <div class="col-sm-2">
+        <div  style="background-color: Green;">
+        Event Calendar
+        </div>
+      </div>
     </div>
   </div>
 </template>
 <script src="./home.ts"></script>
-<style></style>
+<style scoped lang="scss">
+.search-row {
+  padding-top: 5px;
+  padding-bottom: 10px;
+}
+
+.border-it {
+  border: 1px gray solid;
+}
+</style>
