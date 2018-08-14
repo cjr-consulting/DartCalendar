b9cd1b6b81474d7829f641c83f158403a544a92c
John Meade <johnnynibbles@gmail.com>
2018-08-14 10:50:28 -0400

    Updated with stylecop fxcop

--- src/DartCalendar.Web/WebApi/LoginController.cs
+++ src/DartCalendar.Web/WebApi/LoginController.cs
@@ -1,15 +1,16 @@
-using System;
-using Microsoft.AspNetCore.Mvc;
-using System.Threading.Tasks;
-
 namespace DartCalendar.Web.WebApi
 {
+    using System;
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Mvc;
+
     [Route("api/Login")]
     public class LoginController : Controller
     {
         public IActionResult Get()
         {
-            return Json(new {
+            return Json(new
+            {
                 Username = "user@email.io",
                 Id = "1"
             });
